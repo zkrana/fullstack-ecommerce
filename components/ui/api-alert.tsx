6ca9b6386745075b3fa707ff3b7de0cdcdffb2cd
Zk Rana <zkranao@gmail.com>
2023-07-21 16:59:56 +0600

    API ALert Implemented

--- components/ui/api-alert.tsx
+++ components/ui/api-alert.tsx
@@ -1,4 +1,3 @@
-"use client"
 import { Copy, Server } from "lucide-react";
 import { toast } from "react-hot-toast";
 
@@ -35,18 +34,13 @@ export const ApiAlert: React.FC<ApiAlertProps> = ({
 
   return ( 
     <Alert>
-      <Server className="w-4 h-6" />
-
-      <AlertTitle className=" ml-8 w-full flex items-center gap-x-2">
-
+      <Server className="h-4 w-4" />
+      <AlertTitle className="flex items-center gap-x-2">
         {title}
-
         <Badge variant={variantMap[variant]}>
-        {textMap[variant]}
+          {textMap[variant]}
         </Badge>
-
       </AlertTitle>
-
       <AlertDescription className="mt-4 flex items-center justify-between">
         <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
           {description}
